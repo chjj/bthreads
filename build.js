ce7b8b94d5d339da8b72445bf10de6df23998b11
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-01-10 10:17:30 -0800

    browser: add bundle and build.

---
+++ build.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const fs = require('fs');
+const {resolve} = require('path');
+const bpkg = require('bpkg');
+const input = resolve(__dirname, 'lib/browser/bundle.js');
+const output = resolve(__dirname, 'lib/browser/bundle.json');
+
+(async () => {
+  const code = await bpkg({
+    env: 'browser',
+    input: input,
+    plugins: [
+      ['babylonia', {
+        targets: 'last 2 versions'
+      }],
+      ['uglify-es', {
+        toplevel: true
+      }]
+    ]
+  });
+
+  fs.writeFileSync(output, JSON.stringify(code) + '\n');
+})();
