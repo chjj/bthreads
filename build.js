87ffbef16fbc7fe059f2515cf97ff3f2a5a79c66
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-01-10 15:09:27 -0800

    browser: a better way to setup the eval environment.

--- build.js
+++ build.js
@@ -2,25 +2,16 @@
 
 'use strict';
 
-const fs = require('fs');
-const {resolve} = require('path');
-const bpkg = require('bpkg');
-const input = resolve(__dirname, 'lib/browser/bundle.js');
-const output = resolve(__dirname, 'lib/browser/bundle.json');
-
-(async () => {
-  const code = await bpkg({
-    env: 'browser',
-    input: input,
-    plugins: [
-      ['babylonia', {
-        targets: 'last 2 versions'
-      }],
-      ['uglify-es', {
-        toplevel: true
-      }]
-    ]
-  });
-
-  fs.writeFileSync(output, JSON.stringify(code.trim()) + '\n');
-})();
+require('bpkg')({
+  env: 'browser',
+  input: 'lib/browser/bundle.js',
+  output: 'etc/bundle.js',
+  plugins: [
+    ['babylonia', {
+      targets: 'last 2 chrome versions'
+    }],
+    ['uglify-es', {
+      toplevel: true
+    }]
+  ]
+});
