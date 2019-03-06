eec91ac4149d6bd31b8494964df0d28917ec0823
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-03-06 10:31:14 -0800

    pkg: add different entry points.

--- threads.js
+++ threads.js
@@ -1,9 +1,9 @@
 /*!
- * bthreads.js - worker threads for javascript
+ * threads.js - worker threads for javascript
  * Copyright (c) 2019, Christopher Jeffrey (MIT License).
  * https://github.com/bcoin-org/bthreads
  */
 
 'use strict';
 
-module.exports = require('./browser');
+module.exports = require('./lib/threads');
